02df0430d0445c1eec895f4de695ad68150f72b5
sheetalj2205 <sheetalj2205@gmail.com>
2022-06-07 17:50:47 +0530

    13th problem (3rd problem of the day)

--- threeSum.cpp
+++ threeSum.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 void twoSum(vector<int>& arr, int target);
+void Print_Vector(vector<int> Vec);
+
 int main(){
     int n;
     cout<<"Enter array size"<<endl;
@@ -19,29 +21,84 @@ int main(){
 }
 
 void twoSum(vector<int>& arr, int target){
- vector<vector<int>> ans;
- int y;
- int x;
- vector<int> level;
- for(int i = 0; i<arr.size()-3; i++){
-    x = 0 - arr[i];
-   for(int j = i+1; j<arr.size()-2; j++){
-      y = x - arr[j];
-     for(int k = j+1; k<arr.size(); k++){
-         if(arr[k] == y){
-             level.push_back(arr[i]);
-             level.push_back(arr[j]);
-             level.push_back(arr[k]);
-         }
-     }
-   }
-   ans.push_back(level);
-   level.clear();
+//  set<vector<int>> ans;
+ sort(arr.begin(),arr.end());
+//  // brute force approach 
+//  // SC = O(M) (M distinct triplets) and TC = O(N^3)*log M (log M = to insert distinct triplets in set)
+//  for(int i = 0; i<arr.size()-1; i++){
+//    for(int j = i+1; j<arr.size()-1; j++){
+//      for(int k = j+1; k<arr.size(); k++){
+//          if(arr[i] + arr[j]+ arr[k] == 0){
+//              ans.insert({arr[i], arr[j], arr[k]});
+//          }
+//      }
+//    }
+// }
+
+// //HashMap Approach 
+// unordered_map<int, int> mp;
+
+// for(int i = 0; i<arr.size(); i++){
+//     mp[arr[i]]++;
+// }
+
+// for (int i = 0; i<arr.size()-1; i++){
+//     mp[arr[i]]--;
+//     for(int j = i+1; j<arr.size()-1; j++){
+//         mp[arr[j]]--;
+//         int c = arr[i]+arr[j]; 
+//         if(mp.count(-c)>0){
+//             ans.insert({arr[i], arr[j], -c});
+//         }
+//         mp[arr[j]]++;
+
+//     }
+//     mp[arr[i]]++;
+// }
+
+// Two Pointer Approach - most optimized
+vector<vector<int>> ans;
+for(int i  = 0; i<arr.size(); i++){
+    if(i>0 && arr[i] == arr[i-1]){
+        continue; //skip arr[i] if its value is same as previous one
+    }
+    int low = i+1, high = arr.size()-1;
+    while(low<high){
+        int sum = arr[i]+ arr[low]+ arr[high];
+        if(sum == 0){
+            ans.push_back({arr[i], arr[low], arr[high]});
+            int value = arr[low];
+            while(low<arr.size() && value == arr[low]){
+              low++; //skip similar low values
+            }
+            value = arr[high];
+            while(high>0 && value == arr[high]){
+              high--; //skip similar high values
+           }
+        }
+        else if(sum>0){
+            high--;
+        }
+        else{
+            low++;
+        }
+
+    }
 }
- for(int i = 0; i<ans.size(); i++){
-     for(int j = 0; j<3; j++){
-        cout<<ans[i][j]<<" "<<endl;
-     }
-       
- }
+
+
+cout << "Set of Vectors: \n";
+for (auto it = ans.begin(); it != ans.end(); it++)
+{
+    Print_Vector(*it);
+}
+  
+}
+void Print_Vector(vector<int> Vec)
+{
+    for (int i = 0; i < Vec.size(); i++) {
+        cout << Vec[i] << " ";
+    }
+    cout << endl;
+    return;
 }
