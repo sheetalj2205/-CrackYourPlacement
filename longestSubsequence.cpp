43d9ce3cce7bc27ec21dce12ffe169fc2fab4bee
sheetalj2205 <sheetalj2205@gmail.com>
2022-06-15 11:08:55 +0530

    Solved: Memoization+ Recursion approach of LCS

--- longestSubsequence.cpp
+++ longestSubsequence.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int static t[1001][1001]; //matrix for memoization
 int longestSubseq(string word1,int n, string word2, int m);
 int main(){
     string word1, word2;
@@ -10,6 +11,7 @@ int main(){
     cin>>word2;
     int m = word2.size();
     int n = word1.size();
+    memset(t, -1, sizeof(t)); //intialize matrix with -1 for memoization approach
     int ans = longestSubseq(word1, n, word2, m);
     cout<<ans;
 }
@@ -24,6 +26,22 @@ int longestSubseq(string word1, int n, string word2, int m){
     }
     return max(longestSubseq(word1, n, word2, m-1), longestSubseq(word1, n-1, word2, m)); //if chars are not equal then this condition takes max value from both the recursions
     //------------------------------------------------------
+
+
+    //memoization + Recursion (DP)
+    if(n == 0 || m == 0){ //base condition i.e. smallest valid input
+        return 0;
+    }
+    if(t[n][m] != -1){
+        return t[n][m];
+    }
+    else if(word1[n-1] == word2[m-1]){
+        return t[n][m] = 1 + longestSubseq(word1, n-1, word2, m-1); //if both values/chars are equal then check for next chars, adding 1 because of that one similar char
+    }
+    return t[n][m] = max(longestSubseq(word1, n, word2, m-1), longestSubseq(word1, n-1, word2, m));
+
+    //--------------------------------------------------
+
     // top-down approach DP
     int dp[n+1][m+1];
     for(int i = 0; i<n+1; i++){ //use these loops for base condition of recursive solution i.e when either length of word1 or word2 is zero
