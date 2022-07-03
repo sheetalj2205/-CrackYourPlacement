a09fe126d54a44de280bcf93205db3d28272a42a
sheetalj2205 <sheetalj2205@gmail.com>
2022-07-03 13:34:43 +0530

    Solved: Array question

---
+++ Arrays/rearrangeElements.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rearrangeArray(vector<int>& arr);
+
+int main(){
+    int n;
+    cout<<"Enter size of array ";
+    cin>>n;
+    cout<<"Enter array";
+    vector<int> arr(n);
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    rearrangeArray(arr);
+}
+void rearrangeArray(vector<int>& nums){
+    vector<int> v(nums.size());
+        int p = 0, n = 1;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] > 0){
+                v[p] = nums[i];
+                p+=2;
+            }
+            else{
+                v[n] = nums[i];
+                n+=2;
+            }
+        }
+        for(int i = 0; i<nums.size(); i++){
+            cout<<v[i]<<" ";
+        }
+}
